2aedafcd60037e41fe1abc3ab0d7e809c29b9409
Jakub Liput <jakub.liput@gmail.com>
2015-11-27 12:20:16 +0100

    deregister_service script and support in Storage

--- deployment_scripts/storage_manager/stop.sh
+++ deployment_scripts/storage_manager/stop.sh
@@ -10,3 +10,5 @@ execute cd $SCALARM_ROOT/scalarm_storage_manager
 info "Stopping Storage Manager"
 execute rake service:stop_single
 
+info "Registering log_bank in Information Service (using public address: $PUBLIC_LOG_BANK_ADDRESS)"
+execute $SCRIPT_PATH/../deregister_service.sh storage_managers $PUBLIC_LOG_BANK_ADDRESS
