7c253d5d87d2962d0d9dc02402788df34e36056f
Jakub Liput <jakub.liput@gmail.com>
2015-11-29 23:48:18 +0100

    Fixing local deploy and shutdown scripts

--- deployment_scripts/local_deploy.sh
+++ deployment_scripts/local_deploy.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
 source `dirname $0`/utils.sh
 
-cd `dirname $0`
+execute cd $SCRIPT_PATH
 
-# execute information_service/install.sh
-# execute storage_manager/install.sh
-# execute experiment_manager/install.sh
+execute information_service/install.sh
+execute storage_manager/install.sh
+execute experiment_manager/install.sh
 execute data_explorer/install.sh
+execute pathfinder/install.sh
 
-# execute information_service/configure.sh
-# execute storage_manager/configure.sh
-# execute experiment_manager/configure.sh
+execute information_service/configure.sh
+execute storage_manager/configure.sh
+execute experiment_manager/configure.sh
 execute data_explorer/configure.sh
+execute pathfinder/configure.sh
 
-# execute information_service/start.sh
-# execute storage_manager/start.sh
-# execute experiment_manager/start.sh
+execute information_service/start.sh
+execute storage_manager/start.sh
+execute experiment_manager/start.sh
 execute data_explorer/start.sh
+execute pathfinder/start.sh
--- deployment_scripts/local_shutdown.sh
+++ deployment_scripts/local_shutdown.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 source `dirname $0`/utils.sh
 
-cd `dirname $0`
+execute cd $SCRIPT_PATH
 
+execute_optional pathfinder/stop.sh
 execute_optional data_explorer/stop.sh
 execute_optional experiment_manager/stop.sh
 execute_optional storage_manager/stop.sh
 execute_optional information_service/stop.sh
 
+execute_optional pathfinder/delete.sh
 execute_optional data_explorer/delete.sh
 execute_optional experiment_manager/delete.sh
 execute_optional storage_manager/delete.sh
