0a2fdd9e517517f415f2bd5e21338ae6038a441d
Jakub Liput <jakub.liput@gmail.com>
2015-12-02 15:02:27 +0100

    PLGrid certificates install script

---
+++ deployment_scripts/install_plgrid_certs.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+sudo apt-get install -y rpm
+wget https://dist.eugridpma.info/distribution/igtf/current/accredited/RPMS/ca_PolishGrid-1.70-1.noarch.rpm -O plg-certs.rpm
+sudo rpm -i plg-certs.rpm
