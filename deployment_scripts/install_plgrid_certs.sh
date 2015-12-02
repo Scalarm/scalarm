1f0db83404efb9a934e595e947c8401368b8a768
Jakub Liput <jakub.liput@gmail.com>
2015-12-02 15:09:57 +0100

    Forcing plgrid certs RPM installation

--- deployment_scripts/install_plgrid_certs.sh
+++ deployment_scripts/install_plgrid_certs.sh
@@ -2,4 +2,4 @@
 
 sudo apt-get install -y rpm
 wget https://dist.eugridpma.info/distribution/igtf/current/accredited/RPMS/ca_PolishGrid-1.70-1.noarch.rpm -O plg-certs.rpm
-sudo rpm -i plg-certs.rpm
+sudo rpm -i --replacepkgs plg-certs.rpm
