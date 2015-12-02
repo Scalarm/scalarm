04764ba0593f511f4e80da462610c0d7d3a4bf23
Jakub Liput <jakub.liput@gmail.com>
2015-12-02 14:49:17 +0100

    Removing old default overriding envs

--- deployment_scripts/pathfinder/start.sh
+++ deployment_scripts/pathfinder/start.sh
@@ -1,15 +1,6 @@
 #!/bin/bash
 source `dirname $0`/../utils.sh
 
-export RAILS_ENV=production
-
-# Setting default env variables if not defined
-[ -n $INFORMATION_SERVICE_URL ] || export INFORMATION_SERVICE_URL=localhost:11300
-[ -n $INFORMATION_SERVICE_LOGIN ] || export INFORMATION_SERVICE_LOGIN=scalarm
-[ -n $INFORMATION_SERVICE_PASSWORD ] || export INFORMATION_SERVICE_PASSWORD=scalarm1024
-[ -n $SECRET_KEY_BASE ] || export SECRET_KEY_BASE=dfjeiwu38r782781j38nx9
-[ -n $DB_SECRET_KEY ] || export SECRET_KEY_BASE=zxcg6as7d7qh29d5f6fdsa
-
 info "Go to Pathfinder direcotry"
 execute cd $SCALARM_ROOT/scalarm_experiment_supervisor
 
