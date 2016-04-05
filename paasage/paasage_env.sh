3cbf59610f1cdd27f0c25259e2d6b93c027221bb
Jakub Liput <jakub.liput@gmail.com>
2016-04-05 15:13:01 +0200

    Initial paasage env

---
+++ paasage/paasage_env.sh
@@ -0,0 +1,5 @@
+# Using envs set in dockers by passage, set Scalarm env
+
+export PUBLIC_NGINX_ADDRESS=$PUBLIC_IP
+export INFORMATION_SERVICE_URL=$PUBLIC_InfSerPortReq
+export GIT_BRANCH=development
