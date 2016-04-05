969e65286077b28f160a71c99763a64895acd354
Jakub Liput <jakub.liput@gmail.com>
2016-04-05 12:37:44 +0200

    Added fake docker set env

---
+++ paasage/docker_fake_env.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+source `dirname $0`/../utils.sh
+
+info "Setting ENVs for docker"
+export PUBLIC_NGINX_ADDRESS=`bash -c "ifconfig | sed -En 's/127.0.0.1//;s/.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*/\2/p'"`
+export GIT_BRANCH=development
