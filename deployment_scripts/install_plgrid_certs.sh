70de16306be8fee2e9d637184fd24be8fa764976
Dariusz Krol <dkrol@agh.edu.pl>
2016-04-28 15:50:33 +0200

    Updating URL to PLGrid certificates.

--- deployment_scripts/install_plgrid_certs.sh
+++ deployment_scripts/install_plgrid_certs.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-URL=https://dist.eugridpma.info/distribution/igtf/current/accredited/RPMS/ca_PolishGrid-1.72-1.noarch.rpm
+# URL=https://dist.eugridpma.info/distribution/igtf/current/accredited/RPMS/ca_PolishGrid-1.72-1.noarch.rpm
+URL=http://quattorsrv.lal.in2p3.fr/packages/CA/ca-1.72/ca_PolishGrid-1.72-1.noarch.rpm
 
 sudo apt-get install -y rpm
 wget $URL -O plg-certs.rpm
