73d945c80b2032e6967e700b6d77426311f4adfc
Dariusz Krol <dkrol@agh.edu.pl>
2016-04-29 19:44:28 +0200

    Updating command to install rvm and ruby

--- deployment_scripts/install_rvm.sh
+++ deployment_scripts/install_rvm.sh
@@ -6,11 +6,11 @@ then
   info "Ruby already installed: $(ruby --version)"
 else
   info "RVM keys installation"
-  execute gpg --keyserver 213.133.103.71 --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
+  gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
 
   info "RVM installation"
-  execute eval "bash -c 'curl -sSL https://get.rvm.io | bash -s stable --ruby=2.3'"
+  execute eval "curl -sSL https://get.rvm.io | bash -s stable --ruby=2.3'"
 
   info "RVM first load"
-  execute source $HOME/.rvm/scripts/rvm
+  execute source /etc/profile.d/rvm.sh
 fi
