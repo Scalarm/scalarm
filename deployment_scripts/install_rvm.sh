459e14665c9ec89cb1d977b8ef42e3c5628cf002
Dariusz Krol <dkrol@agh.edu.pl>
2016-06-04 15:51:34 +0200

    Fixes in rvm installation

--- deployment_scripts/install_rvm.sh
+++ deployment_scripts/install_rvm.sh
@@ -9,10 +9,11 @@ else
   gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
 
   info "RVM installation"
-  execute eval "curl -sSL https://get.rvm.io | bash -s stable --ruby=2.3"
+  execute eval "curl -sSL https://get.rvm.io | bash -s stable --ruby=2.3.1"
 
   info "RVM first load"
-  execute source /etc/profile.d/rvm.sh
+  execute_optional source /etc/profile.d/rvm.sh
+  execute_optional source ~/.rvm/scripts/rvm
 
   execute eval "gem install bundler"
 fi
