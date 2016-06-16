058dde0016a31fe807b8a05b40ded4d6aeb3ff0e
Dariusz Król <dkrol@agh.edu.pl>
2016-06-16 08:07:19 +0200

    Checking www-data user even if nginx is installed.

--- deployment_scripts/nginx/install.sh
+++ deployment_scripts/nginx/install.sh
@@ -4,6 +4,10 @@ source `dirname $0`/../utils.sh
 info "Go to SCALARM_ROOT: $SCALARM_ROOT"
 execute cd $SCALARM_ROOT
 
+info "Checking www-data user"
+execute sudo id -u www-data &>/dev/null || sudo useradd www-data
+execute sudo id -u www-data &>/dev/null
+
 if nginx -v; then
   echo "nginx already installed at versin $(nginx -v)"
 else
@@ -29,4 +33,4 @@ else
 	fi
 fi
 
-execute_optional sudo useradd www-data
+
