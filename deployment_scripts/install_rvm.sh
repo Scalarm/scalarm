2e62ac36f2074531a68ae7953aff77958c6efd99
Dariusz Król <dkrol@agh.edu.pl>
2016-06-16 08:38:04 +0200

    Fixing load_rvm function

--- deployment_scripts/install_rvm.sh
+++ deployment_scripts/install_rvm.sh
@@ -15,10 +15,11 @@ load_rvm() {
   fi
 }
 
+execute_optional load_rvm
+
 if ruby --version
 then
   info "Ruby already installed: $(ruby --version)"
-  load_rvm()
 else
   info "RVM keys installation"
   gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
@@ -27,7 +28,7 @@ else
   execute eval "curl -sSL https://get.rvm.io | bash -s stable --ruby=2.3.1"
 
   info "RVM first load"
-  load_rvm()
+  execute_optional load_rvm
 
   execute eval "gem install bundler"
 fi
