57cf12b6cca2390818037e994dea82d459470e9c
Dariusz Król <dkrol@agh.edu.pl>
2016-06-16 08:00:17 +0200

    Small fix to support older cents.

--- deployment_scripts/nginx/start.sh
+++ deployment_scripts/nginx/start.sh
@@ -2,4 +2,4 @@
 source `dirname $0`/../utils.sh
 
 info "Execute nginx with Scalarm configuration"
-execute sudo nginx -p $SCALARM_ROOT/nginx-scalarm -c nginx.conf
+execute sudo nginx -p $SCALARM_ROOT/nginx-scalarm/ -c nginx.conf
